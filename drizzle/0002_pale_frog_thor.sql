346c09ca9df736e21123fc66776947034b023302
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-08-13 10:25:41 +0700

    feat: add index in email

---
+++ drizzle/0002_pale_frog_thor.sql
@@ -0,0 +1,2 @@
+CREATE INDEX "email" ON "users" USING btree ("email");--> statement-breakpoint
+CREATE INDEX "id" ON "users" USING btree ("id");
--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,14 +1,18 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { index, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-	id: uuid("id").primaryKey().defaultRandom(),
-	name: varchar({ length: 255 }).notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
-	password: varchar({ length: 255 }).notNull(),
-	emailVerifiedAt: timestamp(),
-	createdAt: timestamp().notNull().defaultNow(),
-	updatedAt: timestamp()
-		.defaultNow()
-		.notNull()
-		.$onUpdate(() => new Date()),
-});
+export const usersTable = pgTable(
+	"users",
+	{
+		id: uuid("id").primaryKey().defaultRandom(),
+		name: varchar({ length: 255 }).notNull(),
+		email: varchar({ length: 255 }).notNull().unique(),
+		password: varchar({ length: 255 }).notNull(),
+		emailVerifiedAt: timestamp(),
+		createdAt: timestamp().notNull().defaultNow(),
+		updatedAt: timestamp()
+			.defaultNow()
+			.notNull()
+			.$onUpdate(() => new Date()),
+	},
+	(table) => [index("email").on(table.email), index("id").on(table.id)],
+);
